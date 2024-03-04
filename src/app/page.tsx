557e7d4e2415942cd6ed752b97f8e51f2688d034
Guilherme Bustamante <guilherme_bustamante@live.com>
2024-03-04 15:04:11 -0300

    feat: add contact section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
-
+import { Separator } from "@/components/ui/separator";
+import ContactSection from "@/sections/contact-section";
 import HeroSection from "@/sections/hero-section";
+import RentSection from "@/sections/rent-section";
 import ServicesSection from "@/sections/services-section";
 export default function Home() {
   return (
-    <main className="relative w-full mx-auto 3xl:max-w-7xl">
+    <main className="relative mx-auto w-full 3xl:max-w-7xl">
       <HeroSection />
 
-      <ServicesSection/>
+      <ServicesSection />
+
+      <RentSection />
+
+      <Separator className="my-5 w-full max-w-7xl mx-auto" />
+
+      <ContactSection />
+
+      <Separator className="my-5 w-full max-w-7xl mx-auto" />
     </main>
   );
 }
---
+++ src/sections/contact-section.tsx
@@ -0,0 +1,31 @@
+import { RiMapPin2Fill } from "react-icons/ri";
+import { MdWhatsapp } from "react-icons/md";
+
+const ContactSection = () => {
+  return (
+    <section className="bg-[url('/Contact.svg')] bg-cover bg-center bg-no-repeat w-full h-auto py-10 md:rounded-xl max-w-7xl mx-auto my-5">
+      <div className="w-full flex flex-col md:flex-row max-w-7xl mx-auto text-white items-center justify-around px-5 md:px-0">
+        <div className="flex flex-col">
+          <span className="uppercase font-light">Localização e Contato</span>
+          <h3 className="text-5xl font-black">BUILD MASTER</h3>
+        </div>
+
+        <div className="flex flex-col items-start justify-start mt-10 md:mt-0 gap-4 bg-black bg-opacity-60 p-5 rounded-xl">
+          <div className="flex items-center gap-4">
+            <RiMapPin2Fill className="text-primary w-12 h-12 md:w-9 md:h-9" />
+            <p className="font-light">
+              Avenida dos Pinheiros, 394 - Parque Industrial, SJC - SP
+            </p>
+          </div>
+
+          <div className="flex items-center gap-4">
+            <MdWhatsapp className="text-primary w-9 h-9" />
+            <p className="font-light">+55 (12) 99999-9999</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ContactSection;
